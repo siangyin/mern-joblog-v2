86f932b85fce9853ed53dcc63bc3db04ceab805f
Lee Siang Yin <siangyin916@gmail.com>
2024-03-15 20:43:27 +0800

    update json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,32 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <h1>home</h1>,
+		errorElement: <h1>error</h1>,
+		children: [
+			{
+				index: true,
+				element: <h1>landing</h1>,
+			},
+			{
+				path: "register",
+				element: <h1>register</h1>,
+			},
+			{
+				path: "login",
+				element: <h1>login</h1>,
+			},
+			{
+				path: "dashboard",
+				element: <h1>dashboard</h1>,
+			},
+		],
+	},
+]);
+
 const App = () => {
-	return <h1>Joblog</h1>;
+	return <RouterProvider router={router} />;
 };
 export default App;
