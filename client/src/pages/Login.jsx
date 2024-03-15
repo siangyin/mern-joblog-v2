a462d44bf6fbb8bd6d3c28d6382a09eace89763e
Lee Siang Yin <siangyin916@gmail.com>
2024-03-15 22:36:11 +0800

    added login and register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,32 @@
+import { Form, Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo, FormRow } from "../components";
+
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<Wrapper>
+			<Form method="post" className="form">
+				<Logo />
+				<h4>login</h4>
+				<FormRow type="email" name="email" />
+				<FormRow type="password" name="password" />
+
+				<button type="submit" className="btn btn-block">
+					Submit
+				</button>
+
+				<button type="button" className="btn btn-block">
+					explore the app
+				</button>
+
+				<p>
+					Not a member yet?
+					<Link to="/register" className="member-btn">
+						Register
+					</Link>
+				</p>
+			</Form>
+		</Wrapper>
+	);
 };
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,31 @@
+import { Form, Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo, FormRow } from "../components";
+
 const Register = () => {
-	return <div>Register</div>;
+	return (
+		<Wrapper>
+			<Form method="post" className="form">
+				<Logo />
+				<h4>Register</h4>
+				<FormRow type="text" name="name" />
+				<FormRow type="text" name="lastName" labelText="last name" />
+				<FormRow type="text" name="location" />
+				<FormRow type="email" name="email" />
+				<FormRow type="password" name="password" />
+
+				<button type="submit" className="btn btn-block">
+					Submit
+				</button>
+
+				<p>
+					Already a member?
+					<Link to="/login" className="member-btn">
+						Login
+					</Link>
+				</p>
+			</Form>
+		</Wrapper>
+	);
 };
 export default Register;
